1bd506376de861612f40ecd117d66b7411968b10
Ghislain Hachey <ghachey@gmail.com>
2014-06-24 16:55:48 +0800

    Upgraded all dev-ops related configuration files and npm packages. Basic test now passing and ready to add real tests. Grunt serve still working.

--- frontend/app/scripts/services/resources.js
+++ frontend/app/scripts/services/resources.js
@@ -3,8 +3,8 @@
 /* HTTP resource services */
 
 //var wsUrl = 'https://candy.pacificpolicy.org';
-//var wsUrl = 'http://localhost:5000'; // for dev, don't commit
-var wsUrl = 'http://freswota:5000';
+var wsUrl = 'http://localhost:5000'; // for dev, don't commit
+//var wsUrl = 'http://freswota:5000';
 //var wsUrl = 'http://economics.pacificpolicy.org';
 
 angular.module('pippDataApp.services.resources', [])
