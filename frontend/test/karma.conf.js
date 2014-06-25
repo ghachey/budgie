77ef5a18fd2ba78c8221a64889af77ccfcff344b
Ghislain Hachey <ghachey@gmail.com>
2014-06-25 11:25:34 +0800

    Removed unused lib in testing

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -23,7 +23,6 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-touch/angular-touch.js',
       'bower_components/angularjs-nvd3-directives/dist/angularjs-nvd3-directives.js',
       'bower_components/angular-bootstrap/ui-bootstrap.min.js',
       'bower_components/underscore/underscore.js',
